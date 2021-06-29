255f326326d490b19d004f8e5b33744fb34d7326
Rafael Simionato Hayashi <rafaelshayashi@gmail.com>
2021-06-28 22:27:19 -0300

    :tada: Initial Commit

---
+++ src/test/java/br/com/rafaelshayashi/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelshayashi.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
